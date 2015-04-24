52d94a9a9bbd2297d90fdd21d4ae858182ca42c1
Jake Archibald <jaffathecake@gmail.com>
2015-04-24 19:04:09 +0100

    Fixing polyfill urls

--- public/js/page/index.js
+++ public/js/page/index.js
@@ -2,16 +2,16 @@ var loadScripts = require("./load-scripts");
 var polyfillsNeeded = [];
 
 if (!window.Promise) { // IE :(
-  polyfillsNeeded.push('js/promise-polyfill.js');
+  polyfillsNeeded.push('/js/promise-polyfill.js');
 }
 
 if (!window.fetch) {
-  polyfillsNeeded.push('js/fetch.js');
+  polyfillsNeeded.push('/js/fetch.js');
 }
 
 // I'm sure user-agent sniffing will be fiiiiine
 if (/(iPhone|iPad);/.test(navigator.userAgent)) {
-  polyfillsNeeded.push('js/fastclick.js');
+  polyfillsNeeded.push('/js/fastclick.js');
 }
 
 loadScripts(polyfillsNeeded, function() {
